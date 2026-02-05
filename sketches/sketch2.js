ad613ec044ec251837820974c159f100e0e76f6e
hoangc04 <hoangc04@uw.edu>
2026-02-04 19:43:14 -0800

    sizing img

--- sketches/sketch2.js
+++ sketches/sketch2.js
@@ -69,7 +69,7 @@ registerSketch('sk2', function (p) {
     const swimmerX = cx + (ringWidth / 2) * p.cos(swimmerAngle);
     const swimmerY = cy + (ringHeight / 2) * p.sin(swimmerAngle);
 
-    const swimmerSize = 150;
+    const swimmerSize = 100;
     p.push();
     p.translate(swimmerX, swimmerY);
     p.rotate(swimmerAngle + p.HALF_PI);
