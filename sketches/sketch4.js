c119e4a92679a8df0eedf5ab1ac4c2cd16fbb171
hoangc04 <hoangc04@uw.edu>
2026-02-05 19:36:14 -0800

    Delete and change img files

--- sketches/sketch4.js
+++ sketches/sketch4.js
@@ -4,7 +4,7 @@ registerSketch('sk4', function (p) {
   let stationImg;
 
   p.preload = function () {
-    stationImg = p.loadImage(`images/trainstation.png`);
+    stationImg = p.loadImage(`images/train-station.png`);
   };
 
   p.setup = function () {
